76f8bd408140ed06c24be67941cc34482fe99908
Cedric Twillie <32856661+ctwillie@users.noreply.github.com>
2023-08-29 23:52:16 -0400

    feat: add rate limiting to the login routes (#40)
    Refs: #35
    
    Co-authored-by: Cedric Twillie <cedric.twillie@1nhealth.com>

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,17 +24,32 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
+        $this->registerRateLimiters();
 
         $this->routes(function () {
+            Route::middleware(['web', 'guest'])
+                ->group(base_path('routes/guest.php'));
+
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+        });
+    }
+
+    /**
+     * Configure the rate limiters for the application.
+     */
+    private function registerRateLimiters(): void
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
+        RateLimiter::for('login', function (Request $request) {
+            return Limit::perMinute(5)->by($request->user()?->id ?: $request->ip());
         });
     }
 }
