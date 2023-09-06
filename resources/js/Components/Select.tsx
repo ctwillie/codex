fd93393749766fe49eaa010fb38e5eb00d01f73e
Cedric Twillie <32856661+ctwillie@users.noreply.github.com>
2023-09-05 23:12:29 -0400

    style: change select list hover styles (#52)

--- resources/js/Components/Select.tsx
+++ resources/js/Components/Select.tsx
@@ -3,6 +3,8 @@ import {
     Select as NextUISelect,
     SelectItem,
     SelectProps as NextUISelectProps,
+    SelectedItems,
+    SelectedItemProps,
 } from "@nextui-org/react";
 
 type SelectProps = Partial<NextUISelectProps> & {
@@ -14,10 +16,10 @@ type SelectProps = Partial<NextUISelectProps> & {
 export default function Select({ options, ...restPops }: SelectProps) {
     return (
         <NextUISelect
+            aria-label="Select"
             size="md"
             radius="sm"
             variant="bordered"
-            color="primary" // TODO: change theme colors
             labelPlacement="outside"
             scrollShadowProps={{
                 isEnabled: false,
@@ -28,15 +30,36 @@ export default function Select({ options, ...restPops }: SelectProps) {
                     "dark:bg-gray-900 hover:dark:bg-gray-900 border-gray-300 dark:border-gray-700 dark:text-white",
                 popover: "dark:bg-gray-900 dark:text-gray-100",
             }}
+            renderValue={(items: SelectedItems) => {
+                return items.map((i: SelectedItemProps) => {
+                    const { textValue, value } = i.props as Record<string, any>;
+
+                    return <div key={`render-value-${value}`}>{textValue}</div>;
+                });
+            }}
+            listboxProps={{
+                itemClasses: {
+                    base: [
+                        "text-foreground",
+                        "transition-opacity",
+                        "data-[hover=true]:!text-white",
+                        "data-[hover=true]:bg-indigo-500",
+                        "dark:data-[hover=true]:bg-indigo-500",
+                        "dark:data-[selectable=true]:focus:bg-indigo-500",
+                        "dark:data-[selectable=true]:!text-white",
+                        "data-[focus-visible=true]:ring-indigo-500",
+                    ],
+                },
+            }}
             {...restPops}
         >
             {options.map((option) => (
                 <SelectItem
                     key={option.value}
                     value={option.value}
-                    color="primary"
+                    textValue={option.label}
                 >
-                    {option.label}
+                    <div>{option.label}</div>
                 </SelectItem>
             ))}
         </NextUISelect>
