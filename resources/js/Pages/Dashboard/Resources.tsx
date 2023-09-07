ffbce965d2fa1fdd80e2d568f34dbf7dee615bec
Cedric Twillie <32856661+ctwillie@users.noreply.github.com>
2023-09-06 23:40:11 -0400

    feat: make resource search form an accordion (#58)

--- resources/js/Pages/Dashboard/Resources.tsx
+++ resources/js/Pages/Dashboard/Resources.tsx
@@ -11,6 +11,7 @@ import ResourceListItem from "@/Pages/Dashboard/Resources/ResouceListItem";
 import { SelectOption, TechnologySelectOption } from "@/types/select";
 import EmptyState from "@/Components/EmptyState";
 import CreateResourceModal from "@/Pages/Dashboard/Resources/CreateResourceModal";
+import { Accordion, AccordionItem } from "@nextui-org/react";
 
 type ResourcesProps = {
     resources: Resource[];
@@ -63,13 +64,55 @@ export default function Resources({
             <Tabs tabs={tabs} />
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                    <Card>
-                        <ResourceSearchForm
-                            categorySelectOptions={categorySelectOptions}
-                            technologySelectOptions={technologySelectOptions}
-                        />
-                    </Card>
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 rounded-lg">
+                    <Accordion
+                        className="dark:bg-gray-800"
+                        variant="bordered"
+                        keepContentMounted
+                    >
+                        <AccordionItem
+                            key="1"
+                            aria-label="Search"
+                            indicator={
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-6 h-6 text-gray-500"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
+                                    />
+                                </svg>
+                            }
+                            title={
+                                <>
+                                    <p className="inline">Search</p>
+                                    <p className="px-4 text-sm text-gray-500 inline">
+                                        Find what you're looking for
+                                    </p>
+                                </>
+                            }
+                            classNames={{
+                                title: "dark:text-gray-100 pl-0 sm:pl-2 lg:pl-8",
+                            }}
+                        >
+                            <div className="px-0 sm:px-4 lg:px-8 pb-6 lg:pb-10">
+                                <ResourceSearchForm
+                                    categorySelectOptions={
+                                        categorySelectOptions
+                                    }
+                                    technologySelectOptions={
+                                        technologySelectOptions
+                                    }
+                                />
+                            </div>
+                        </AccordionItem>
+                    </Accordion>
 
                     <Card>
                         <div className="flex justify-end mb-12">
